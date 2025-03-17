b5a908b68ee1edf9e9e0dd3419575bc737bddf58
surendra1572 <160829110+surendra1572@users.noreply.github.com>
2025-03-17 16:34:50 +0530

    add

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull surendra157/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 surendra157/simple-python-flask-app
+docker run -d -p 8000:8000 surendra157/simple-python-flask-app   
